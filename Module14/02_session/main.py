a391eeb8bd3cd08cb2ba777fe13b734f3eb236f1
Игорь Е с работы <toermakov@mail.ru>
2022-09-22 14:36:11 +0300

    task_02_complete

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+try:
+    k = y_diff / x_diff
+except:
+    print('Угловой коэффициент равен: 0! (координаты точек по оси Х одинаковы')
+    k = 0
+
+b = y2 - k * x2
+
+print("Уравнение прямой, проходящей через эти точки:")
+print("y = ", k, "* x + ", b)
