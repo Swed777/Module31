462f6887ac73ebf34503a43525d2679484d64f78
Игорь Е с работы <toermakov@mail.ru>
2022-09-23 14:10:48 +0300

    task_05_is_complete

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+
+number = int(input('Введите натуральное число больше 1: '))
+
+for i in range(number, 1, -1):
+    if number % i == 0 :
+        min_divider = i
+print(f'Наименьший делитель числа {number} = {min_divider}')
