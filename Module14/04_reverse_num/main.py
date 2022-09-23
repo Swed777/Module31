9cebb0c8d1d1cfa888f6e02b5fb6e39d33973e54
Игорь Е с работы <toermakov@mail.ru>
2022-09-23 13:39:37 +0300

    task_04_is_complete

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,24 +1,18 @@
-def rotate(x):  # переворот числа наоборот
+def rotate(x):  # переворот части числа наоборот
     rotate_num = 0
     while x != 0:
         num = x % 10
         rotate_num = (rotate_num * 10) + num
         x = x // 10
-    print(rotate_num)
     return (rotate_num)
-def integer(x):
-    intgr = int(x)
-    rotate(intgr)
-    return(intgr)
-
-def decimal(x):  # Находим дробную часть
-    dec = int((x % 1) * 100)
-    rotate(dec)
-    return()
-
 
 n = float(input('Введите первое вещественное число: '))
+rotate_n_int = rotate(int(n))                    # Переворачиваем целую часть числа
+rotate_n_dec = rotate(int((n % 1) * 100))        # Переворачиваем дробную часть
 k = float(input('Введите второе вещественное число: '))
+rotate_k_int = rotate(int(k))                    # Переворачиваем целую часть числа
+rotate_k_dec = rotate(int((k % 1) * 100))        # Переворачиваем дробную часть
 
-print('\nПервое число наоборот: ', integer(n), decimal(n))
-print('\nВторое число наоборот: ', decimal(k), decimal(k))
+print('\nПервое число наоборот: ', (rotate_n_int + rotate_n_dec/100))
+print('Второе число наоборот: ', (rotate_k_int + rotate_k_dec/100))
+print('Сумма:                 ', (rotate_n_int + rotate_n_dec/100) + (rotate_k_int + rotate_k_dec/100))
