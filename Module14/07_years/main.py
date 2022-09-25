1d3d18a81a3738f49f59db0ac09b14c7979dda8e
Игорь Е с работы <toermakov@mail.ru>
2022-09-26 00:47:50 +0300

    task_07_is_work

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,27 @@
 # TODO здесь писать код
+
+def year(year_min, year_max):
+    for x in range(year_min, year_max + 1):
+        similar_num(x)
+'''def similar_num(x):
+    one = x // 1000
+    two = x % 1000 // 100
+    three = x % 100 // 10
+    four = x % 10
+    if one == two
+'''
+def similar_num(x):
+    count = 0
+    x = str(x)
+    for i in x:
+        for j in (x):
+            if i == j:
+                count += 1
+                if count >= 3:
+                    print(x)
+
+year_min = int(input('Введите первый год: '))
+year_max = int(input('Введите второй год: '))
+
+print(f'Годы от {year_min} до {year_max} с тремя одинаковыми цифрами:')
+year(year_min, year_max)
