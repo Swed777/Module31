cbd7b70eb86ea14903ec0d971617867c6cbc8467
Игорь Е с работы <toermakov@mail.ru>
2022-10-14 16:38:20 +0300

    task_07_start

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+skates = int(input('\nКол-во коньков: '))
+skates_list = []
+for i in range(skates):
+    skates_size = int(input(f'Размер {i + 1}-й пары: '))
+    skates_list.append(skates_size)
+
+people = int(input('\nКол-во людей: '))
+people_list = []
+for i in range(people):
+    leg_size = int(input(f'Размер ноги {i + 1}-го человека: '))
+    people_list.append(leg_size)
+
+print(skates_list, people_list)
+
+people_quantity = 0
+print('\nНаибольшее кол-во людей, которые могут взять ролики: ', people_quantity)
