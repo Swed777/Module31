9a9e2dbcde8127da9dcafd06101bb1c164007156
Игорь Е с работы <toermakov@mail.ru>
2022-10-17 21:22:09 +0300

    task_07_comlete

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -11,6 +11,6 @@ for i in range(people):
     leg_size = int(input(f'Размер ноги {i + 1}-го человека: '))
     people_list.append(leg_size)
 
-result= list(set(skates_list) & set(people_list))       # Находим пересечение двух списков
+result= list(set(skates_list) & set(people_list))       # Находим пересечение  двух списков
 
 print('\nНаибольшее кол-во людей, которые могут взять ролики: ', len(result))
