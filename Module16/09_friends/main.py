469ad0c2cfcc184e3af681b0435b7e12bcc133df
Игорь Е с работы <toermakov@mail.ru>
2022-10-24 22:05:43 +0300

    task_09_comlete

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+
+N = int(input('Введите количество друзей:           '))
+frends_list = [0] * N           # Формируем список из N  друзей с нулевым балансом у каждого
+K = int(input('Введите количество долговых распиок: '))
+print()
+
+for receipt in range(1, K + 1):
+    print(f'\n{receipt}-я расписка: ')
+    for_people = int(input('Кому:     '))   # отрицаельно, - тот, кто деньги должен вернуть
+    from_people = int(input('От кого:  '))  # положительно, - тот, кто деньги должен получить
+    how_march = int(input('Сколько?  '))
+    frends_list[for_people - 1] -= how_march    # Вычитаем у должника
+    frends_list[from_people - 1] += how_march   # ДОбавляем получателю
+
+print('Баланс друзей: ')
+for i in range(len(frends_list)):
+    print(i + 1 , ':', frends_list[i])
