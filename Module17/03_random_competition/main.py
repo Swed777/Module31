7e2bb4d9290bd6b7551ec68c5771d0d6f2613574
Игорь Е с работы <toermakov@mail.ru>
2022-10-26 19:05:42 +0300

    task_03_complete

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+import random
+
+first = [round(random.uniform(5, 10), 2) for _ in range(20)]
+print('Первая команда: ', first)
+second = [round(random.uniform(5, 10), 2) for _ in range(20)]
+print('Вторая команда: ', second)
+winners = [float(first[i])  if first[i] > second[i] else float(second[i]) for i in range(20)]
+print('Победители тура', winners)
