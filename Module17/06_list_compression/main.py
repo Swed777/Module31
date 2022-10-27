62f01f7c0f9792c6650d22f0cf75d71f77b0a782
Игорь Е с работы <toermakov@mail.ru>
2022-10-27 19:06:28 +0300

    task_06_complete

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+
+#  !!!!!!  Никита - прошу проверить, правильно ли я понял алгоритм задачи? !!!!!
+
+import random
+
+number = int(input('Количество чисел в списке: '))
+original_list = [random.randint(0,2) for _ in range(number)]
+print('Список до сжатия', original_list)
+print('Список после сжатия: ', [i for i in original_list if i > 0])
