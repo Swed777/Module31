daf3cd7209ab674d7f2c58ca84b9015eda881e9a
Игорь <toermakov@mail.ru>
2022-10-31 00:45:40 +0300

    task_07_correct  task_09_complete

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -2,3 +2,5 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
 # TODO здесь писать код
+
+print([two for one in nice_list for two in [b for a in one for b in a]])
