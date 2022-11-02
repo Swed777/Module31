abe53f73649ff55647db2eee9200babdbf6d6fcc
Игорь Е с работы <toermakov@mail.ru>
2022-11-02 17:29:49 +0300

    task_06_inProgress

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+
+text = input('Введите строку: ')
+
+result, temp = [], []
+last_elem = text[0]
+
+for elem in text:               # Находим список повторяющиеся элементы в строке
+    if elem == last_elem:
+        temp.append(elem)
+    else:
+        result.append(temp)     # Формируем список из списков повторяющихся элементов
+        temp = [elem]
+        last_elem = elem
+result.append(temp)
+
+print(temp, len(temp), temp[0])
+print(result, len(result))
+
+# [temp.append(elem) if elem == last_elem for elem in text]
