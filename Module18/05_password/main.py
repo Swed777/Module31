6c6039c23e0a3e766267e3af9eb4d3d2f5c70f72
Игорь Е с работы <toermakov@mail.ru>
2022-11-02 13:59:15 +0300

    task_05_complete

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+
+count_dig   = 0
+count_upper = 0
+
+while True:
+    password = input('Придумайте пароль: ')
+    if len([count_dig + 1 for i in password if i in '0123456789']) < 3:   # Проходим по паролю, определяем количество цифр
+        print('Пароль ненадежный. Попробуйте еще раз. ')
+    elif password.islower() == True:
+        print('Пароль ненадежный. Попробуйте еще раз. ')
+    else:
+        print('Это надёжный пароль!')
+        break
+
+
+
