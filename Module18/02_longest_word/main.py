ccc9077cb3eeb61830f0e76e567c390758c34024
Игорь Е с работы <toermakov@mail.ru>
2022-11-02 09:24:02 +0300

    task_02_complete

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,22 +1,12 @@
 # TODO здесь писать код
 
 text = input("Введите строку: ")
-max_word = max([word for word in text.split()])
-max_word_long = max([len(word) for word in text.split()])
-
-print('Самое длинное слово: {0} \nДлина этого слова: {1}'.format(max_word, max_word_long))
-
-# ***********************
-'''
-text = input('Введите строку: ').split()
 max_len = 0
 max_word = ''
- 
-for word in text:
-    if len(word) > max_len:
-        max_len = len(word)
-        max_word = word
- 
-print('Самое длинное слово:', max_word)
-print('Длина этого слова:', max_len)
-'''
+
+for word in text.split():
+      if len(word) > max_len:
+            max_len = len(word)
+            max_word = word
+
+print('Самое длинное слово: {0} \nДлина этого слова: {1}'.format(max_word, max_len))
