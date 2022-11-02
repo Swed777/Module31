d81ec6d3dcce95e55e358556f4f62d0928f339a6
Игорь Е с работы <toermakov@mail.ru>
2022-11-02 11:28:13 +0300

    task_03_complete

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+def control(file):
+    pref = '@№$%^&*()'
+    ext = ('.txt', '.docx')
+    for i in pref:
+        if file.startswith(i) == True:
+            print("Ошибка: название начинается на один из специальных символов --> {}".format(i))
+            return
+    if not file.endswith(ext):
+        print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx.')
+    else:
+        print("Файл назван верно")
+
+name = input('Название файла: ')
+control(name)
