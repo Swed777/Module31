ded874566204e5407373326d74ca9d09b15f51a0
Игорь Е с работы <toermakov@mail.ru>
2022-11-11 14:29:03 +0300

    task_09_complete

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,8 +1,23 @@
 # TODO здесь писать код
 
-text = input('Сообщение: ')
-print('Новое сообщение: ')
+text = input('Сообщение: ').split()
+punktuation = ',.!?:;'
+result = []
 
-for word in text.split():
-    new_text = word[::-1]
-    print(new_text, end=' ')
+print('Новое сообщение: ', end='')
+
+for word in text:
+    new_word = ''
+    new_word_all = ''
+    new_text_all = ''
+    for letter in word:
+        if not letter in punktuation:
+                new_word += letter
+        else:
+                new_word_all += new_word[::-1] + letter
+                new_word = ''
+    new_word_all += new_word[::-1]
+    new_text_all += new_word_all
+    result.append(new_text_all)
+
+print(' '.join(result))
