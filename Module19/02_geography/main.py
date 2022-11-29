11f4221bd58a107ab5fdea6b9bb53751f92013bf
Игорь Е с работы <toermakov@mail.ru>
2022-11-29 12:44:31 +0300

    task_03_complete

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -3,6 +3,7 @@
 quantity = int(input('Количество стран: '))
 country_city = {}
 
+# Формируем словать, при этом страна является ключем, а города - значением. По каждой стране - 3 пары
 for i in range(quantity):
     country = input(f'Название {i + 1} страны и 3 городов: ').split()
     for city in country[1:]:
