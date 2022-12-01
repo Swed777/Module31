b04ca39389c071e76d85255f1ad2d26d24b9c11c
Игорь <toermakov@mail.ru>
2022-12-01 21:11:20 +0300

    task_06_comlete

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -3,13 +3,11 @@
 number = int(input('Введите количество пар слов: '))
 synonim = {}
 
-for i in range(1, number + 1):
+for i in range(1, number + 1):                      # Формируем словарь из заданного количества пар
     duble = input(f'{i}-я пара: ').lower().split('-')
     synonim[duble[0].strip()] = duble[1].strip()
 
-print(list(synonim.items()))
-
-while True:
+while True:                                         # Проверяем вхождение заданного слова в словарь
     word = input('Введите слово: ').lower().strip()
     for key, value in synonim.items():
              if word == key:
