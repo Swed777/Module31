1cdfa4932bb752e22dce1b9ba32f69c407236cf3
Игорь <toermakov@mail.ru>
2022-12-07 21:43:48 +0300

    task_08_inProcess

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,7 @@
 # TODO здесь писать код
+max_number = int(input('Введите максимальное число: '))
+
+need_number = input('Нужное число есть среди вот этих чисел: ').split()
+many = set(need_number)
+
+print(many)
