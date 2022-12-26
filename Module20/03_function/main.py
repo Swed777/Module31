9be2983694dd996306f1012536e8273f5e1009bc
Игорь Е с работы <toermakov@mail.ru>
2022-12-26 17:00:39 +0300

    task_03_inProcess

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+def slicer(tup, num):
+    # new_tuple = (tup.index(tup[1]))
+    new_tuple = tup[tup.index(tup[1]):]
+    return new_tuple
+
+
+print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
+
+print('________________')
+# tuple_1 = ((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2)
+# print(tuple_1[1])
+# print([i for i, value in enumerate(tuple_1[0])])
