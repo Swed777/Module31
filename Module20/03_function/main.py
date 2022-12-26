5c733ea1247f30b1358ee983dd61ccc24d97465b
Игорь Е с работы <toermakov@mail.ru>
2022-12-26 17:29:54 +0300

    task_03_inProcess

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -8,6 +8,20 @@ def slicer(tup, num):
 print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
 
 print('________________')
-# tuple_1 = ((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2)
+tuple_1 = ((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2)
 # print(tuple_1[1])
-# print([i for i, value in enumerate(tuple_1[0])])
+# print([i for i, value in enumerate(tuple_1[0])] if value = (tuple_1[1]))
+count = 0
+for index, value in enumerate(tuple_1[0]):
+    if value == (tuple_1[1]):
+        start = index
+        count += 1
+        if count == 2:
+            stop = index
+            break
+print(start, stop)
+
+
+
+# Сделать цикл по 1 кортежу, определить индексы 1 и второго элемента. ДЛя отсечки
+# использовать флаг (до 2 взведений)
