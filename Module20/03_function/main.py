548c333d95e372712df6726c473266e60043dd41
Игорь Е с работы <toermakov@mail.ru>
2022-12-27 16:53:41 +0300

    task_03_inProcess

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,6 +1,5 @@
 # TODO здесь писать код
 def slicer(tup, num):
-    # new_tuple = (tup.index(tup[1]))
     new_tuple = tup[tup.index(tup[1]):]
     return new_tuple
 
@@ -9,19 +8,34 @@ print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
 
 print('________________')
 tuple_1 = ((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2)
-# print(tuple_1[1])
-# print([i for i, value in enumerate(tuple_1[0])] if value = (tuple_1[1]))
-count = 0
+
+# ---Определяем первый и последний одинаковый элемент в кортеже
+start, stop = 0, 0
+count = 0               # Признак наличия элемента в кортеже
+flag = False
+new_tuple_1 = tuple()
 for index, value in enumerate(tuple_1[0]):
     if value == (tuple_1[1]):
-        start = index
-        count += 1
-        if count == 2:
-            stop = index
+        if flag == False:
+            flag = True
+            count += 1
+            start = index
+        else:
+            stop = index + 1
+            count += 1
             break
-print(start, stop)
+print(start, stop)    # Убрать после отладки
+
+if count == 0:
+    new_tuple_1 = ()
+elif count == 1:
+    new_tuple_1 = tuple_1[0][start:]
+elif count == 2:
+    new_tuple_1 = tuple_1[0][start:(stop)]
+
+print(new_tuple_1)
 
 
-
-# Сделать цикл по 1 кортежу, определить индексы 1 и второго элемента. ДЛя отсечки
-# использовать флаг (до 2 взведений)
+"""def slicer(tup, num):
+    new_tuple = tup[tup.index(tup[1]):]
+    return new_tuple"""
