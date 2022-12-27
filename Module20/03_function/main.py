b18dab1b17d577aabc0467e6f031292695fbfd31
Игорь Е с работы <toermakov@mail.ru>
2022-12-27 17:05:58 +0300

    task_03_complete

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,41 +1,29 @@
 # TODO здесь писать код
 def slicer(tup, num):
-    new_tuple = tup[tup.index(tup[1]):]
+    # ---Определяем первый и последний одинаковый элемент в кортеже
+    start, stop = 0, 0
+    count = 0  # Признак наличия элемента в кортеже
+    flag = False
+    new_tuple = tuple()
+    for index, value in enumerate(tup):
+        if value == (num):
+            if flag == False:
+                flag = True
+                count += 1
+                start = index
+            else:
+                stop = index + 1
+                count += 1
+                break
+
+# Выводим ответ в нужном виде
+    if count == 0:
+        new_tuple = ()
+    elif count == 1:
+        new_tuple = tup[start:]
+    elif count == 2:
+        new_tuple = tup[start:(stop)]
+
     return new_tuple
 
-
 print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
-
-print('________________')
-tuple_1 = ((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2)
-
-# ---Определяем первый и последний одинаковый элемент в кортеже
-start, stop = 0, 0
-count = 0               # Признак наличия элемента в кортеже
-flag = False
-new_tuple_1 = tuple()
-for index, value in enumerate(tuple_1[0]):
-    if value == (tuple_1[1]):
-        if flag == False:
-            flag = True
-            count += 1
-            start = index
-        else:
-            stop = index + 1
-            count += 1
-            break
-print(start, stop)    # Убрать после отладки
-
-if count == 0:
-    new_tuple_1 = ()
-elif count == 1:
-    new_tuple_1 = tuple_1[0][start:]
-elif count == 2:
-    new_tuple_1 = tuple_1[0][start:(stop)]
-
-print(new_tuple_1)
-
-
-"""def slicer(tup, num):
-    new_tuple = tup[tup.index(tup[1]):]
-    return new_tuple"""
