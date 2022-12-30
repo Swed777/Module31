07009a960b3dcd435ca126721d80fbe525f7daa0
Игорь Е с работы <toermakov@mail.ru>
2022-12-30 15:03:46 +0300

    task_06_complete

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -5,12 +5,11 @@ def rand_l():
     [rand_list.append(random.randint(0, 9)) for i in range(10)]
     print('Оригинальный лист: ', rand_list)
     list_1, list_2 = rand_list[::2], rand_list[1::2]   # ФОрмируем списки из нечетных и четных позиций
-    return list_1, list_2,
+    return list_1, list_2
 
 def pair_l(list_1, list_2):
     new_list = []
-    for pair in zip(list_1, list_2):
-        new_list.append(pair)
+    [new_list.append(pair) for pair in zip(list_1, list_2)]
     return new_list
 
 import random       #randint()	Возвращает случайное число в пределах заданного промежутка
