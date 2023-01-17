3621114c0357f15a86d3aa92e9e7edab1b016a95
Игорь Е с работы <toermakov@mail.ru>
2023-01-17 11:42:58 +0300

    task_06_correct_complete

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,8 +1,9 @@
 # TODO здесь писать код
 
 def rand_l():
-    rand_list = []
-    [rand_list.append(random.randint(0, 9)) for i in range(10)]
+    # rand_list = []                - мой изначальный код, неверный
+    # [rand_list.append(random.randint(0, 9)) for i in range(10)]
+    rand_list = [random.randint(0, 9) for i in range(10)]    # код после корректировки
     print('Оригинальный лист: ', rand_list)
     list_1, list_2 = rand_list[::2], rand_list[1::2]   # ФОрмируем списки из нечетных и четных позиций
     return list_1, list_2
