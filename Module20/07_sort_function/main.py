d1d5a09fbcec3bc1353c7ff1246c331e327a9e78
Игорь Е с работы <toermakov@mail.ru>
2023-01-18 16:08:58 +0300

    task_07_correct_complete

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,10 +1,14 @@
 # TODO здесь писать код
 def tpl_sort(lst):
-    new_tpl_sort = list(lst)
-    return tuple(sorted(new_tpl_sort))
+    for i in lst:
+        if isinstance(i, int) == False:
+            print(isinstance(i, int))
+            return lst
+    else:
+            new_tpl_sort = list(lst)
+            return tuple(sorted(new_tpl_sort))
 
 print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
-
 
 
 
