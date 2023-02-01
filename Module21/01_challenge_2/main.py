7d8d88505f3e2945541d62ae6e478a839ba58036
Игорь Е с работы <toermakov@mail.ru>
2023-02-01 14:16:23 +0300

    01_tack_comlete

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+
+def pr_num(i, num):
+    if i == num + 1:
+        return
+    else:
+        print(i)
+        pr_num(i + 1, num)
+
+num = int(input('Введите num: '))
+i = 1
+pr_num(i, num)
