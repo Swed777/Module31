1a5a2b8e80dcffa7558a83271a51caabc950f32f
Игорь Е с работы <toermakov@mail.ru>
2023-02-10 15:16:39 +0300

    05_tack_complete

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,3 +1,34 @@
+# TODO оптимизировать функцию
+
+def calculating_math_func(data : int, factorial = {0 : 1}):
+    if data in factorial:
+        result = factorial.get(data)
+    else:
+        for index in range(max(factorial) + 1, data + 1):
+            result = factorial.get(max(factorial))
+            result *= index
+            factorial.update({index: result})
+        return calculating_math_func(data, factorial)
+
+    result /= data ** 3
+    result = result ** 10
+    return result
+
+
+number = int(input('Введите число:'))
+result = calculating_math_func(number)
+print(result)
+
+
+'''
+Задача 5. Ускоряем работу функции
+Что нужно сделать
+
+У нас есть функция, которая делает определённые действия с входными данными:
+
+берёт факториал от числа;
+результат делит на куб входного числа;
+получившееся число возводит в 10-ю степень.
 def calculating_math_func(data):
     result = 1
     for index in range(1, data + 1):
@@ -5,5 +36,7 @@ def calculating_math_func(data):
     result /= data ** 3
     result = result ** 10
     return result
-
-# TODO оптимизировать функцию
+Однако каждый раз нам приходится делать сложные вычисления, хотя входные и выходные данные одни и те же. И тут наши знания тонкостей Python должны нам помочь.
+Оптимизируйте функцию так, чтобы высчитывать факториал для одного и того же числа только один раз.
+Подсказка: вспомните, что происходит с изменяемыми данными, если их выставить по умолчанию в параметрах функции.
+'''
