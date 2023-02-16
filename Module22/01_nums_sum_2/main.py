40508c60e0eb93592231eb05979fd1a42bbcede4
Игорь Е с работы <toermakov@mail.ru>
2023-02-16 16:04:56 +0300

    01_task_comlete

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,37 @@
 # TODO здесь писать код
+
+file = open('numbers.txt', 'r')
+sum = 0
+array = tuple()
+
+for i_line in file:
+    num_line = tuple(i_line.split())
+    array += num_line
+print(array)
+
+for i in array:
+    sum += int(i)
+
+file.close()
+
+file_2 = open('answer.txt', 'w')
+file_2.write(str(sum))
+file_2.close()
+
+#  Печатаем для проверки
+print('Сумма чисел в файле = ', sum)
+
+'''
+Задача 1. Сумма чисел 2
+Что нужно сделать
+Во входном файле numbers.txt записано N целых чисел, которые могут быть разделены пробелами и концами строк. Напишите программу, которая выводит сумму чисел в выходной файл answer.txt.
+Пример:
+Содержимое файла numbers.txt
+     2
+2
+  2
+        2
+
+Содержимое файла answer.txt
+8
+'''
