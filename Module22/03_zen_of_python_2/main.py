e24c1b9a53a6536c2bb635b07f804e3fa3bf7620
Игорь Е с работы <toermakov@mail.ru>
2023-02-17 16:09:36 +0300

    03_task_complete

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,13 +1,47 @@
 # TODO здесь писать код
 import os
 
-path = os.path.abspath('zen.txt')
-print(path)
-file = open('zen.txt', 'r')
-# text_of_file = file.readlines()
+# Вариант открытия файла по абсолютному пути
+# file = open('/Users/ei/Desktop/Linux/Python_basic/Module22/Module22/02_zen_of_python/zen.txt', 'r')
+
+file = open(os.path.join('..', '02_zen_of_python' , 'zen.txt'), 'r') # Открытие файла чере относительный путь
+text_of_file = file.readlines()
+num_of_sumbol = 0
+num_of_words = 0
+num_of_stroke = 0
+letters = {}
+alfa_beta = 'abcdefghijklmnopqrstuvwxyz'
+min_letter = ''
+
+for i_str in text_of_file:
+    num_of_sumbol += (len(i_str) - i_str.count(' ')) # Подсчитываем длину символов в каждой строке и вычитаем пробелы
+    num_of_words += len(i_str.split(' '))
+    num_of_stroke += 1
+
+    dict_letter = {}             # формируем словарь букв по каждой строчке
+    for letter in i_str.lower():
+        if letter in alfa_beta:
+            dict_letter[letter] = i_str.count(letter)
+
+    for k, v in dict_letter.items():     #добавляем буквы из конкретной строчки в общую копилку
+        if k in letters:
+            letters[k] += v
+        else:
+            letters[k] = v
 
 
-pass
+min_nunber_letter = min(letters.values())       #Определяем минимальное значение в общем словаре букв
+
+print('Количество букв в файле:',   num_of_sumbol)
+print('Количество слов в файле:',   num_of_words)
+print('Количество строк в файле:',  num_of_stroke)
+for k, v in letters.items():  # если значение конкретного элемента словаря равно минимальному значению
+    if v ==  min_nunber_letter:
+        print('Наиболее редкая буква: ',   k)
+
+file.close()
+
+
 '''
 Задача 3. Дзен Пайтона 2
 Что нужно сделать
