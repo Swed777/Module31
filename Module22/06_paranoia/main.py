8e6834214d165251d61269e818f96791a82e7143
Игорь Е с работы <toermakov@mail.ru>
2023-03-01 16:55:15 +0300

    03_task_complete

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -5,15 +5,12 @@ import string
 # Шифр цезаря
 with open('text.txt', 'r', encoding='utf8') as text:
     for delta, text_translate in enumerate(text.readlines(), 1):
-        lower = (string.ascii_lowercase[delta:] + string.ascii_lowercase[:delta])
-        translate = {ord(a): d for (a, d) in zip(string.ascii_letters, (lower + lower.upper()))}
-        result = str.translate(text_translate, translate)
-
-        print(result)
-
-
-
-
+            lower = (string.ascii_lowercase[delta:] + string.ascii_lowercase[:delta])
+            translate = {ord(a): d for (a, d) in zip(string.ascii_letters, (lower + lower.upper()))}
+            result = str.translate(text_translate, translate)
+            with open('cipher_text.txt', 'a', encoding='utf8') as out_cifer:
+                out_cifer.write(result)
+                print(result)
 
 
 
