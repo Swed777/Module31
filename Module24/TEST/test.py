13c2d3d30307c52cd73123c2f44772a4f3e325bd
Игорь Е с работы <toermakov@mail.ru>
2023-03-20 17:02:22 +0300

    task_02_inProcess

--- Module24/TEST/test.py
+++ Module24/TEST/test.py
@@ -1,5 +1,4 @@
 class Student:
-
         fio = ''
         num_of_group = ''
         grade = tuple()
@@ -8,11 +7,19 @@ def list_group():
             Student.num_of_group   = input('Введите номер группы: ')
             Student.grade = tuple(input('Введите 5 оценок через пробел: ').split())
             return Student.fio, Student.num_of_group, Student.grade
-def give_average():
-        return round(sum(Student.grade) / len(Student.grade), 2)
+# def give_average():
+#         return round(sum(Student.grade) / len(Student.grade))
 
 list_student = []
+summ = 0
 for _ in range(2):
      list_student.append(list_group())
      # list_student.sort(key=lambda x: x.give_average())
+     print(Student.grade)
+     for i in Student.grade:    # находим сумму баллов для каждого студента
+        summ += int(i)
+     print(summ)
 print(list_student)
+
+
+
