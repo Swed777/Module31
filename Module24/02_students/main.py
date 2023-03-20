a8cf02d86ed603c8b7b116ea7072fbc6b7c8240d
Игорь Е с работы <toermakov@mail.ru>
2023-03-20 17:06:05 +0300

    task_02_inProcess

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,27 +1,29 @@
-# TODO здесь писать код
 class Student:
-    def __init__(self, fio, num_of_group, grade):
-        self.fio = fio
-        self.num_of_group = num_of_group
-        self.grade = grade
+    fio = ''
+    num_of_group = ''
+    grade = tuple()
 
-    def __str__(self):
-        return ('Список групп:', self.fio, self.num_of_group, self.grade)
+
 def list_group():
-    name = input('Введите имя и фамилию: ')
-    fi   = input('Введите номер группы: ')
-    ball = tuple(input('Введите 5 оценок через пробел: ').split())
-    return (name, fi, ball)
+    Student.fio = input('Введите имя и фамилию: ')
+    Student.num_of_group = input('Введите номер группы: ')
+    Student.grade = tuple(input('Введите 5 оценок через пробел: ').split())
+    return Student.fio, Student.num_of_group, Student.grade
 
+
+# def give_average():
+#         return round(sum(Student.grade) / len(Student.grade))
 
 list_student = []
+summ = 0
 for _ in range(2):
-    x = Student(*list_group())
-    list_student.append(x)
-print('Список студентов: ')
+    list_student.append(list_group())
+    # list_student.sort(key=lambda x: x.give_average())
+    print(Student.grade)
+    for i in Student.grade:  # находим сумму баллов для каждого студента
+        summ += int(i)
+    print(summ)
 print(list_student)
-for i_stud in list_student:
-     print(i_stud)
 
 
 '''
