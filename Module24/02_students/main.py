5b7dc65c6116f4b01b4a8d031688ee5b01198321
Игорь Е с работы <toermakov@mail.ru>
2023-03-22 15:04:46 +0300

    task_02_inProcess

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -8,6 +8,7 @@ def list_group():
     Student.fio = input('Введите имя и фамилию: ')
     Student.num_of_group = input('Введите номер группы: ')
     Student.grade = tuple(input('Введите 5 оценок через пробел: ').split())
+    Student.grade = sum([int(i) for i in Student.grade]) # преобразование в int
     return Student.fio, Student.num_of_group, Student.grade
 
 
@@ -20,9 +21,6 @@ for _ in range(2):
     list_student.append(list_group())
     # list_student.sort(key=lambda x: x.give_average())
     print(Student.grade)
-    for i in Student.grade:  # находим сумму баллов для каждого студента
-        summ += int(i)
-    print(summ)
 print(list_student)
 
 
