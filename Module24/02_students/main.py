051322e80e8bad9190f802ab49131b3d1f1fddf4
Игорь Е с работы <toermakov@mail.ru>
2023-03-27 11:59:46 +0300

    task_02_сomplete

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -2,21 +2,24 @@ class Student:
     fio = ''
     num_of_group = ''
     grade = tuple()
+
 def list_group():
         Student.fio = input('Введите имя и фамилию: ')
         Student.num_of_group = int(input('Введите номер группы: '))
         Student.grade = tuple(input('Введите 5 оценок через пробел: ').split())
-        Student.grade_median = sum([int(i) for i in Student.grade])/5 # преобразование в int
+        Student.grade_median = sum([int(i) for i in Student.grade])/5 # преобразование в int и среднее значение
         return Student.fio, Student.num_of_group, Student.grade, Student.grade_median
 
 list_student = []
 summ = 0
-for _ in range(2):
+for _ in range(10):              # ФОрмируем список  помощью объектов класса
     list_student.append(list_group())
-print('Список студентов первоначальный: ')
-print(list_student)
+
 print('Список студентов отсортированный:')
-print(list_student.sort(key=lambda x: x[3]))
+print('|Ф.И.О.| Гр |        Оценки      |')
+list_student.sort(key=lambda x: x[3])  # Сортируем список по среднему значению
+for x in list_student:
+    print(*x[:3])   # Выводим список на экран Без среднего значения
 
 
 
@@ -26,5 +29,4 @@ print(list_student.sort(key=lambda x: x[3]))
 Реализуйте модель с именем Student, содержащую поля «ФИ», «Номер группы», «Успеваемость» (список из пяти элементов).
 Затем создайте список из десяти студентов (данные о студентах можете придумать или запросить у пользователя) 
  и отсортируйте список по возрастанию среднего балла. Выведите результат на экран.
-
 '''
