ff93b98bce9a8c26816a8281855ada290cfa36ca
Игорь <toermakov@mail.ru>
2023-03-29 23:03:27 +0300

    task_04_complete

--- Module24/04_magic/main.py
+++ Module24/04_magic/main.py
@@ -1,12 +1,29 @@
 # TODO здесь писать код
 class Water:
     name = 'Вода'
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm.name
+        elif isinstance(other, Fire):
+            return Steam.name
+        elif isinstance(other, Earth):
+            return Dirt.name
+
 class Air:
     name = 'Воздух'
-class Fire
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lighting.name
+        elif isinstance(other, Earth):
+            return Dust.name
+class Fire:
     name = 'Огонь'
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            return Lava.name
 class Earth:
     name = 'Земля'
+
 class Storm:
     name = 'Шторм'
 class Steam:
@@ -19,8 +36,21 @@ class Dust:
     name = 'Пыль'
 class Lava:
     name = 'Лава'
+    # def __str__(self):
+        # return "Тест на память"
 
-pass
+
+print(Water() + Air())
+print(Water() + Fire())
+print(Water() + Earth())
+print(Air() + Fire())
+print(Air() + Earth())
+print(Fire() + Earth())
+
+# print(Earth() + Fire()) -  для сложения при перестановке слагаемых нужно прописать в классах также обратные действия. Если нужно - пропишу
+
+
+# print(Lava()) # для задания лишнее, оставил на память для изучения метода __str__(self)
 
 '''
 Задача 4. Магия
