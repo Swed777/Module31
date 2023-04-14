6ebe11879cad0cd31d57b7e285d623bb0c765b59
Игорь Е с работы <toermakov@mail.ru>
2023-04-14 10:08:43 +0300

    task_06_complete

--- Module24/06_tic-tac-toe/main.py
+++ Module24/06_tic-tac-toe/main.py
@@ -1,197 +1,135 @@
-# TODO здесь писать код
 from colorama import init, Fore
 init(autoreset=True)
-class Board:
-    board = list(range(9))   # определяем общее состояние поля из 9 ячеек
-    def border_field(self):       # и выводим его на печать
-        print(Fore.GREEN + '- ' * 7)
-        for i_cell in range(3):
-            print((Fore.GREEN + '|'), (Fore.LIGHTYELLOW_EX + str(self.board[0 + i_cell * 3])),
-                   Fore.GREEN + '|',  (Fore.LIGHTYELLOW_EX + str(self.board[1 + i_cell * 3])),
-                   Fore.GREEN + '|',  (Fore.LIGHTYELLOW_EX + str(self.board[2 + i_cell * 3])), Fore.GREEN + '|')
-            print(Fore.GREEN + '- ' * 7)
-
-    def change_cell(self, number_cell):
-        if number_cell != 'X'.lower() or number_cell != 'O'.lower():
-            number_cell =  777 # Добавить информацию о конкретном элементе Х или О
-            return True
-        else:return False
-
-    def victory_check(board):
-        victory_line = ((0, 1, 2), (3, 4, 5), (6, 7, 8),
-                        (0, 3, 6), (1, 4, 7), (2, 5, 8),
-                        (0, 4, 8), (2, 4, 6))
-        for i in victory_line:
-            if Board.board[i[0]] == Board.board[i[1]] == Board.board[i[2]]:  # Проверяем наличие одинаковых символов Х или О в выигрышных линиях
-                return True
-        return False
-#
 class Cell:
     states = {0: ' ', 1: 'X', 2: 'O'}
-    def __init__(self, index):
-        self.index = int(index)
+
+    def __init__(self, index: int):
+        self.index = index
         self.state = 0
-    def cell_status(self):
+
+    def __str__(self):
         if self.state == 0:
-            print(f'Данная ячейка ->{self.index} свободна.')
+            result = f'{self.index}'
+        else:
+            result = f'{Cell.states[self.state]}'
+        return result
+
+    def set_status(self, state: states):
+        if self.state == 0:
+            self.state = state
+        self.print_state()
+
+    def is_free(self) -> bool:
+        if self.state == 0:
             return True
-        print(f'Данная ячейка ->{self.index} занята.')
+        print('Данная ячейка занята.')
         return False
+
+    def print_state(self):
+        print(f'Ячейка {self.index} сейчас {Cell.states[self.state]}.')
+
+
 class Player:
- def __init__(self, name: str):
+    def __init__(self, name: str):
         self.name = name
         self.figure: Cell.states = 0
-        self.num_of_victory = 0
         self.my_turn = False
- def set_symbol(self, symbol):
+
+    def set_symbol(self, symbol: str):
         if symbol == 'X':
             self.figure = 1
         else:
             self.figure = 2
-# class Game:
 
 
-
-print('*****************')
-contur = Board()
-contur.border_field()
-Board.victory_check(1)
-
-
-'''
-Задача 6. Крестики-нолики
-Что нужно сделать
-Создайте программу, которая реализует игру «Крестики-нолики».
-
-Для этого напишите:
-1. Класс, который будет описывать поле игры.
-class Board:
-    #  Класс поля, который создаёт у себя экземпляры клетки.
-    #  Пусть класс хранит информацию о состоянии поля (это может быть список из девяти элементов).
-    #  Помимо этого, класс должен содержать методы:
-    #  «Изменить состояние клетки». Метод получает номер клетки и, если клетка не занята, меняет её состояние. Если состояние удалось изменить, метод возвращает True, иначе возвращается False.
-    #  «Проверить окончание игры». Метод не получает входящих данных, но возвращает True/False. True — если один из игроков победил, False — если победителя нет.
-2. Класс, который будет описывать одну клетку поля:
-class Cell:
-    #  Клетка, у которой есть значения:
-    #  занята она или нет;
-    #  номер клетки;
-    #  символ, который клетка хранит (пустая, крестик, нолик).
-3. Класс, который описывает поведение игрока:
-class Player:
-    #  У игрока может быть:
-    #  имя,
-    #  количество побед.
-    #  Класс должен содержать метод:
-    #   «Сделать ход». Метод ничего не принимает и возвращает ход игрока (номер клетки). Введённый номер нужно обязательно проверить.
-4. Класс, который управляет ходом игры:
-class Game:    
-    # класс «Игры» содержит атрибуты:
-    # состояние игры,
-    # игроки,
-    # поле.
-    # А также методы:
-    # Метод запуска одного хода игры. Получает одного из игроков, запрашивает у игрока номер клетки, изменяет поле, проверяет, выиграл ли игрок. Если игрок победил, возвращает True, иначе False.
-    # Метод запуска одной игры. Очищает поле, запускает цикл с игрой, который завершается победой одного из игроков или ничьей. Если игра завершена, метод возвращает True, иначе False.
-    # Основной метод запуска игр. В цикле запускает игры, запрашивая после каждой игры, хотят ли игроки продолжать играть. После каждой игры выводится текущий счёт игроков.
-'''
-'''
-from colorama import init, Fore
-init(autoreset=True)
 class Board:
     def __init__(self):
-        self.board = [Cell(index) for index in range(1, 10)]                 # Пусть класс хранит информацию о состоянии поля (это может быть список из девяти элементов).
-    def board_field(self):  # определяем общее состояние поля из 9 ячеек
+        self.cells = [Cell(index) for index in range(1, 10)]
+        self.battle = True
+
+    def check_win(self):
+        win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        for cell in win_coord:
+            sum_row = self.cells[cell[0]].state + self.cells[cell[1]].state + self.cells[cell[2]].state
+            if sum_row > 0:
+                if self.cells[cell[0]].state == self.cells[cell[1]].state == self.cells[cell[2]].state:
+                    self.battle = False
+                    return True
+        return False
+
+    def draw_board(self):
+        print("-" * 13)
+        for i in range(3):
+            print("|", self.cells[0 + i * 3], "|", self.cells[1 + i * 3], "|", self.cells[2 + i * 3], "|")
+            print("-" * 13)
+
+    def draw_board(self):  # определяем общее состояние поля из 9 ячеек
         print(Fore.GREEN + '- ' * 7)
         for i_cell in range(3):
-            print((Fore.GREEN + '|'), (Fore.LIGHTYELLOW_EX + str(self.board[0 + i_cell * 3])),
-                   Fore.GREEN + '|',  (Fore.LIGHTYELLOW_EX + str(self.board[1 + i_cell * 3])),
-                   Fore.GREEN + '|',  (Fore.LIGHTYELLOW_EX + str(self.board[2 + i_cell * 3])), Fore.GREEN + '|')
+            print((Fore.GREEN + '|'), (Fore.LIGHTYELLOW_EX + str(self.cells[0 + i_cell * 3])),
+                  Fore.GREEN + '|', (Fore.LIGHTYELLOW_EX + str(self.cells[1 + i_cell * 3])),
+                  Fore.GREEN + '|', (Fore.LIGHTYELLOW_EX + str(self.cells[2 + i_cell * 3])), Fore.GREEN + '|')
             print(Fore.GREEN + '- ' * 7)
-    def change_cell(self, num_cell_player):   #  «Изменить состояние клетки». Метод получает номер клетки и, если клетка не занята,
-        if num_cell_player in range(1, 10):   #  меняет её состояние. Если состояние удалось изменить, метод возвращает True, иначе возвращается False.
-            return '***'
-            # if str(Board.board[num_cell_player]) not in 'XO':
-            #     return True
-        else:
-            print('Клетка уже занята')
-            return False
-    def finish_game_status(self):  #  «Проверить окончание игры». Метод не получает входящих данных, но возвращает True/False.
-                                    # True — если один из игроков победил, False — если победителя нет.
-        self.victory_line = ((1, 2, 3), (4, 5, 6), (7, 8, 9),
-                             (1, 4, 7), (2, 5, 8), (3, 6, 9),
-                              (1, 5, 9), (3, 5, 7))
-        for i in self.victory_line:
-            if Board.board[i[0]] == Board.board[i[1]] == Board.board[i[2]]:  # Проверяем наличие одинаковых символов Х или О в выигрышных линиях
-                return True
-            else:
-                return False
-    def __str__(self):
-        return f'Набор ячеек в таблице выглядит вот так: \n {self.board}'
-class Cell:
-    def __init__(self, index, free_cell_status = 0):
-        self.free_cell_status = free_cell_status         #  свободна (0) она или нет (1);
-        self.number_cell = int(index)                    #  номер клетки;
-        self.symbol_cell = {0:' ', 1:'X', 2:'O'}          #  символ, который клетка хранит (пустая, крестик, нолик).{0:' ', 1:'X', 2:'O'}
-    def __str__(self):
-        if self.free_cell_status == 0:
-            result = f'{self.number_cell}'
-        else:
-            result = f'{self.symbol_cell[2]}, не равен'  # !!! нужно подставить номер игрока
-        return result
+
+    def count_free_cell(self):
+        """ Есть ли у нас свободные ячейки """
+        count = 0
+        for cell in self.cells:
+            if cell.state == 0:
+                count += 1
+        return count
+
+    def turn(self, player: Player, cell: int):
+        self.cells[cell].set_status(player.figure)
+        if self.check_win():
+            print(f'{player.name}, ходивший фигурой "{Cell.states[player.figure]}" победил.')
+            self.draw_board()
+        if self.count_free_cell() == 0:
+            self.draw_board()
+            self.battle = False
+            print('\nБольше нет свободных ячеек.\nВ этот раз ничья.')
 
 
-class Player:
-    name = ''                                    # имя,
-    count_victory = 0                            # количество побед.
-    def __init__(self, symbol):
-        self.symbol = symbol
+p1 = Player(input('Введите имя первого игрока: '))
+p2 = Player(input('Введите имя второго игрока: '))
 
-    def make_move(self):             #   «Сделать ход». Метод ничего не принимает и возвращает ход игрока (номер клетки).
-        return Board.change_cell()   #   ??? Введённый номер нужно обязательно проверить. ??? Е.И. - По условиям задачи клетку мы уже проверяем в классе Board
+while True:
+    answer = input(f'\nЧем будет ходить {p1.name}:\n1 - X\n2 - O\nВыбор: ')
+    if answer == '1':
+        p1.figure = 1
+        p2.figure = 2
+        break
+    elif answer == '2':
+        p1.figure = 2
+        p2.figure = 1
+        break
 
-class Game:
-    def __init__(self):
-        self.game_status = True               # состояние игры,
-        self.player1 = Player('1 игрок')      # игроки,
-        self.player2 = Player('2 игрок')
-        self.field_status = 0                 # поле.
+while True:
+    answer = input(f'\nКто будет ходить первым:\n1 - {p1.name}\n2 - {p2.name}\nВыбор: ')
+    if answer == '1':
+        p1.my_turn = True
+        break
+    elif answer == '2':
+        p2.my_turn = True
+        break
 
-    # Метод запуска одного хода игры. Получает одного из игроков, запрашивает у игрока номер клетки,
-    # изменяет поле, проверяет, выиграл ли игрок. Если игрок победил, возвращает True, иначе False.
-    def one_step(self):
-        self.pl = int(input('Кто сейчас ходит:? \n1 - первый игрок \n2 - второй игрок\n'))
-        Cell.number_cell = int(input('Введите номер клетки для хода:\n'))
-        if self.pl == 1:
-            Cell.symbol_cell = 'X'
-        else:
-            Cell.symbol_cell = 'O'
-        Board.change_cell(Cell.number_cell)
+brd = Board()
 
-    # Метод запуска одной игры. Очищает поле, запускает цикл с игрой, который завершается победой
-    # одного из игроков или ничьей. Если игра завершена, метод возвращает True, иначе False.
-
-    # Основной метод запуска игр. В цикле запускает игры, запрашивая после каждой игры,
-    # хотят ли игроки продолжать играть. После каждой игры выводится текущий счёт игроков.
-    def main(self):
-        while True:
-            pass
-
-#  Отладка:
-test = Board()
-print(test)
-test.board_field()
-# print(test.finish_game_status())
-# xxx = Game()
-# xxx.one_step()
-cell1 = Cell(7, 0)
-print(cell1)
-
-
-print(Cell(3, 1))
-
-# print('\n1 игрок ходит X, второй - O (буква О англ)'
-
-
-
-'''
+while True:
+    brd.draw_board()
+    if p1.my_turn:
+        my_cell = int(input(f'{p1.name} ходит в ячейку: '))
+        if brd.cells[my_cell - 1].is_free():
+            brd.turn(p1, my_cell - 1)
+            p1.my_turn = False
+            p2.my_turn = True
+    elif p2.my_turn:
+        my_cell = int(input(f'{p2.name} ходит в ячейку: '))
+        if brd.cells[my_cell - 1].is_free():
+            brd.turn(p2, my_cell - 1)
+            p1.my_turn = True
+            p2.my_turn = False
+    else:
+        print('Я тогда поиграю за вас.')
+    if not brd.battle:
+        break
