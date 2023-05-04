9d77fe1e5da9fa4d1b5b173da060dc64c4a71add
Nikita Fomin <fominquant@yandex.ru>
2023-05-04 21:34:25 +0300

    Module 25 homework passed

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -4,31 +4,44 @@ class Property:
     Property: базовый класс, описывает имущество налогоплательщиков. Использует стоимость имущества и метод начисления налога
     attr: worth: стоимость имущества
     """
+
     def __init__(self, worth):
         self.worth = worth
+
     def get_worth(self):
         return self.worth
+
     def tax(self, quoter):
         self.quoter = quoter
         """ Property: Алгоритм расчета налога. Для разных видов имущества задается отдельно в дочернем классе """
         return self.worth / self.quoter
+
+
 class Apartment(Property):
     name = 'Апартаменты'
+
     def tax_is(self):
-        t =  self.tax(1000)
+        t = self.tax(1000)
         return t
+
+
 class Car(Property):
     name = 'Автомобиль'
+
     def tax_is(self):
-        t =  self.tax(200)
+        t = self.tax(200)
         return t
+
+
 class CountryHouse(Property):
     name = 'Дача'
+
     def tax_is(self):
         t = self.tax(500)
         return t
 
-money   = float(input('Сколько у Вас денег на уплату налогов?'))
+
+money = float(input('Сколько у Вас денег на уплату налогов?'))
 price_a = float(input('Цена апартаментов:'))
 price_c = float(input('Цена машины:'))
 price_h = float(input('Цена дачи:'))
@@ -44,38 +57,4 @@ print('\nОбщая сумма налога равна : ', sum_tax)
 if sum_tax > money:
     print(f'\n*****   Вам не хватит --> {abs(sum_tax - money)} <-- денег на налоги. Идите работать! *****')
 
-
-
-
-'''
-Задача 1. Налоги
-Что нужно сделать
-Реализуйте иерархию классов, описывающих имущество налогоплательщиков. Она должна состоять из базового класса Property
-и производных от него классов Apartment, Car и CountryHouse.
-
-Базовый класс должен иметь атрибут worth (стоимость), который передаётся в конструктор, и метод расчёта налога, 
-переопределённый в каждом из производных классов. 
-Налог на квартиру вычисляется как 1/1000 её стоимости, на машину — 1/200, на дачу — 1/500. 
-
-Каждый дочерний класс должен иметь конструктор с одним параметром, передающий свой параметр конструктору базового класса.
-Разработайте интерфейс программы. Пусть она запрашивает у пользователя количество его денег и стоимость имущества, 
-а затем выдаёт налог на соответствующее имущество и показывает, сколько денег ему не хватает (если это так).
-
-********** Базовый старт
-class Property:
-    def __init__(self, worth):
-        self.__worth = worth
-
-    def get_worth(self):
-        return self.__worth
-
-    def set_worth(self):
-        return self.__worth
-
-    def tax(self, quoter):
-        return self.__worth / quoter
-
-kv = Property(5000000)
-print(kv.tax(1000))
-
-'''
+# зачтено
--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,27 +1,41 @@
 # TODO здесь писать код
-import random
+
+
 class Buddhist:
     def __init__(self, karma=0):
         self.karma = karma
+
     def get_karma(self):
         return self.karma
+
     def set_karma(self, more_carma):
         self.karma += more_carma
+
+
 class KillError(Exception):
     def __str__(self):
         return 'Убийство'
+
+
 class DrunkError(Exception):
     def __str__(self):
         return 'Напился'
+
+
 class CarCrashError(Exception):
     def __str__(self):
         return 'Разбил машину'
+
+
 class GluttonyError(Exception):
     def __str__(self):
         return 'Лопнул от переедания'
+
+
 class DepressionError(Exception):
     def __str__(self):
         return 'От депрессии прыгнул со скалы'
+
 
 def one_day(day_number):
     if random.randint(1, 10) == 7:
@@ -31,6 +45,7 @@ def one_day(day_number):
             raise misconduct
     else:
         return random.randint(1, 7)
+
 
 buddhist = Buddhist()
 day = 0
@@ -68,28 +83,43 @@ DepressionError.
 закрываем файл
 '''
 import random
+
+
 class Buddhist:
     def __init__(self, karma=0):
         self.karma = karma
+
     def get_karma(self):
         return self.karma
+
     def set_karma(self, more_carma):
         self.karma += more_carma
+
+
 class KillError(Exception):
     def __str__(self):
         return 'Убийство'
+
+
 class DrunkError(Exception):
     def __str__(self):
         return 'Напился'
+
+
 class CarCrashError(Exception):
     def __str__(self):
         return 'Разбил машину'
+
+
 class GluttonyError(Exception):
     def __str__(self):
         return 'Лопнул от переедания'
+
+
 class DepressionError(Exception):
     def __str__(self):
         return 'От депрессии прыгнул со скалы'
+
 
 def one_day(day_number):
     if random.randint(1, 10) == 7:
@@ -99,6 +129,7 @@ def one_day(day_number):
             raise misconduct
     else:
         return random.randint(1, 7)
+
 
 buddhist = Buddhist()
 day = 0
@@ -110,28 +141,4 @@ while buddhist.get_karma() < 500:
         buddhist.set_karma(one_day(day))
 print(('Просветлен!!!'))
 
-'''
-Задача 2. Карма
-Что нужно сделать
-Один буддист-программист решил создать свой симулятор жизни, в котором нужно набрать 500 очков кармы (это константа), 
-чтобы достичь просветления.
-Каждый день вызывается специальная функция one_day(), которая возвращает количество кармы от 1 до 7 и может
-с вероятностью 1 к 10 выкинуть одно из исключений:
-KillError,
-DrunkError,
-CarCrashError,
-GluttonyError,
-DepressionError.
-(Исключения нужно создать самостоятельно, при помощи наследования от Exception.)
-Напишите такую программу. Функцию оберните в бесконечный цикл, выход из которого возможен только при накоплении
-кармы до уровня константы. 
-Исключения обработайте и запишите в отдельный лог karma.log.
-По итогу у вас может быть примерно такая структура программы:
-открываем файл
-цикл по набору кармы
-   try
-      карма += one_day()
-   except(ы) с указанием классов исключений, которые нужно поймать
-      добавляем запись в файл
-закрываем файл
-'''
+# зачтено
