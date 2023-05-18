487151ea4281e8859837c39e1a9c7ccebf446301
Игорь Е с работы <toermakov@mail.ru>
2023-05-18 11:37:29 +0300

    task02_inProcess

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,17 +1,19 @@
 # TODO здесь писать код
 import time
 import requests
+from typing import Callable, Any
 import functools
 
-# def slow_func(func):
-#     @functools.wraps(func)
-#     def wrapper(*args, **kwargs):
-#         time.sleep(5)          # Перед выполнением функции сделаем задержку на 5 секунд
-#         result = func(*args, **kwargs)
-#         return result
-#     return wrapper()
+def slow_func(func):
+    # @functools.wraps(func)
+    # def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs):
+        time.sleep(5)          # Перед выполнением функции сделаем задержку на 5 секунд
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
-# @slow_func
+@slow_func
 def change_site(site : str) -> str:
     response = requests.get(site)
     print('Данные получены')
@@ -19,8 +21,10 @@ def change_site(site : str) -> str:
     response.close()
     return result
 
-test = change_site('http://alexgyver.ru')
-print(test)
+site = str(('http://alexgyver.ru'))
+# test = change_site('http://alexgyver.ru')
+# print(test)
+print(type(change_site(site)))
 
 
 '''
