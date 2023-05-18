d160968fe293944526757aa9fe03e12be6cb59be
Nikita Fomin <fominquant@yandex.ru>
2023-05-18 16:29:15 +0300

    Module 27 homework passed

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,18 +1,22 @@
 # TODO здесь писать код
-import time
-import requests
 import functools
+import time
+
+import requests
+
 
 def slow_func(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        time.sleep(5)          # Перед выполнением функции сделаем задержку на 5 секунд
+        time.sleep(5)  # Перед выполнением функции сделаем задержку на 5 секунд
         result = func(*args, **kwargs)
         return result
+
     return wrapper
 
+
 @slow_func
-def requests_site(site : str) -> str:
+def requests_site(site: str) -> str:
     """
     Функция запроса информации на сайте
     """
@@ -22,19 +26,11 @@ def requests_site(site : str) -> str:
     response.close()
     return result
 
+
 site = str('http://alexgyver.ru')
 test = requests_site(site)
 print(test)
 
-print(requests_site.__doc__)     # Проверка работы  @functools.wraps
+print(requests_site.__doc__)  # Проверка работы  @functools.wraps
 
-
-
-'''
-Задача 2. Замедление кода
-Что нужно сделать
-В программировании иногда возникает ситуация, когда работу функции нужно замедлить.
- Типичный пример — функция, которая постоянно проверяет, изменились ли данные на веб-странице или её код.
-
-Реализуйте декоратор, который перед выполнением декорируемой функции ждёт несколько секунд.
-'''
+# зачтено
--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,50 +1,41 @@
 # TODO здесь писать код
 from datetime import datetime
 
+
 def logging(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         try:
             print(f'{func.__name__} - {func.__doc__}')
-            func()
+            return func(*args, **kwargs)
         except Exception as e:
-            e = f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - {func.__name__} - {e}\n'   # время срабатывания и название функуии с ошибкой в
+            e = f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - {func.__name__} - {e}\n'  # время срабатывания и название функуии с ошибкой в
             with open('function_error.log', 'a+', encoding='utf-8') as fnc:
                 fnc.write(e)
             print(e)
 
     return wrapper
 
+
 @logging
 def zerodiv() -> None:
     """Деление на ноль"""
     x = 1 / 0
+
 
 @logging
 def varname() -> None:
     """Присвоим не существующую переменную"""
     x = y
 
+
 @logging
 def just() -> None:
     """Присваение переменной значения"""
     x = int('Hello World!!!')
 
+
 zerodiv()
 varname()
 just()
 
-
-
-pass
-'''
-Задача 3. Логирование
-Что нужно сделать
-Реализуйте декоратор logging, который будет отвечать за логирование функций. На экран выводится название функции 
-и её документация. Если во время выполнения декорируемой функции возникла ошибка, 
-то в файл function_errors.log записываются название функции и ошибки. 
-
-Также постарайтесь сделать так, чтобы программа не завершалась после обнаружения первой же ошибки, 
-а обрабатывала все декорируемые функции и сразу записывала все ошибки в файл.
-
-Дополнительно: запишите дату и время возникновения ошибки, используя модуль datetime.
-'''
+# зачтено
