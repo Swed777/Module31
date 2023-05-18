5bf51839ae31bee758e6e28296996429d8c420d5
Игорь Е с работы <toermakov@mail.ru>
2023-05-18 12:50:58 +0300

    task03_comlete

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,5 +1,37 @@
 # TODO здесь писать код
+from datetime import datetime
 
+def logging(func):
+    def wrapper():
+        try:
+            print(f'{func.__name__} - {func.__doc__}')
+            func()
+        except Exception as e:
+            e = f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - {func.__name__} - {e}\n' # время срабатывания и название функуии с ошибкой в
+            with open('function_error.log', 'a+', encoding='utf-8') as fnc:
+                fnc.write(e)
+            print(e)
+
+    return wrapper
+
+@logging
+def zerodiv() -> None:
+    """Деление на ноль"""
+    x = 1 / 0
+
+@logging
+def varname() -> None:
+    """Присвоим не существующую переменную"""
+    x = y
+
+@logging
+def just() -> None:
+    """Присваение переменной значения"""
+    x = int('Hello World!!!')
+
+zerodiv()
+varname()
+just()
 
 
 
