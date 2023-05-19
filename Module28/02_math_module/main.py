9b7dadc1848124e21f52cf6addce4b483b368ff4
Игорь <toermakov@mail.ru>
2023-05-20 01:06:06 +0300

    task02_inProcess -вынести в отдельный класс

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,20 +1,37 @@
 # TODO здесь писать код
-class MyMath:
+import math
+from abc import ABC
 
-    # вычисление длины окружности,
-    # вычисление площади окружности,
-    # вычисление объёма куба,
-    # вычисление площади поверхности сферы.
+class MyMath(ABC):
+    """
+    Абстрактный класс MyMath
+    """
+    @classmethod
+    def circle_len(cls, radius: int) -> float:          # вычисление длины окружности,
+        return 2 * math.pi * radius
+
+    @classmethod
+    def circle_sq(cls, radius: int) -> float:               # вычисление площади круга,
+        return math.pi * radius ** 2
+
+    @classmethod
+    def cube_value(cls, rib: int) -> float:                    # вычисление объёма куба,
+        return rib ** 3
+
+    @classmethod
+    def sphere_area(cls, radius: int) -> float:                    # вычисление площади поверхности сферы.
+        return 4 * math.pi * radius ** 2
+
 
 res_1 = MyMath.circle_len(radius=5)
 res_2 = MyMath.circle_sq(radius=6)
-res_3 = MyMath.cube_value(edge=6)
-res_4 = MyMath.sphere_area(radius=6)
+res_3 = MyMath.cube_value(rib=7)
+res_4 = MyMath.sphere_area(radius=8)
 
-print(res_1)
-print(res_2)
-print(res_3)
-print(res_4)
+print(round(res_1, 2))
+print(round(res_2, 2))
+print(round(res_3, 2))
+print(round(res_4, 2))
 
 '''
 Задача 2. Математический модуль
