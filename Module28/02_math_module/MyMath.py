82d17c57e3279db9a04145c586781141241b3d60
Nikita Fomin <fominquant@yandex.ru>
2023-05-22 13:43:59 +0300

    Module 28 homework passed

--- Module28/02_math_module/MyMath.py
+++ Module28/02_math_module/MyMath.py
@@ -1,22 +1,24 @@
 import math
 from abc import ABC
 
+
 class MyMath(ABC):
     """
     Абстрактный класс MyMath
     """
+
     @classmethod
-    def circle_len(cls, radius: int) -> float:          # вычисление длины окружности,
+    def circle_len(cls, radius: int) -> float:  # вычисление длины окружности,
         return 2 * math.pi * radius
 
     @classmethod
-    def circle_sq(cls, radius: int) -> float:               # вычисление площади круга,
+    def circle_sq(cls, radius: int) -> float:  # вычисление площади круга,
         return math.pi * radius ** 2
 
     @classmethod
-    def cube_value(cls, rib: int) -> float:                    # вычисление объёма куба,
+    def cube_value(cls, rib: int) -> float:  # вычисление объёма куба,
         return rib ** 3
 
     @classmethod
-    def sphere_area(cls, radius: int) -> float:                    # вычисление площади поверхности сферы.
+    def sphere_area(cls, radius: int) -> float:  # вычисление площади поверхности сферы.
         return 4 * math.pi * radius ** 2
--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,8 +1,8 @@
 # TODO здесь писать код
-from datetime import date
 
-class Date():
-    def __init__(self, day : int = 0, month : int = 0, year : int = 0):
+
+class Date:
+    def __init__(self, day: int = 0, month: int = 0, year: int = 0):
         self.day = day
         self.month = month
         self.year = year
@@ -11,14 +11,14 @@ class Date():
         return f'День: {self.day}, месяц: {self.month}, год: {self.year}'
 
     @classmethod
-    def split_date(cls, date : str) -> None:
+    def split_date(cls, date: str) -> None:
         lst_date = date.split('-')
         if len(lst_date) != 3:
             print('Неверный формат даты. \nИспользуйте дефис в качестве разделителя')
         cls.day, cls.month, cls.year = map(int, lst_date)
 
     @classmethod
-    def is_date_valid(cls, date : str) -> bool:
+    def is_date_valid(cls, date: str) -> bool:
         cls.split_date(date)
         if 1 <= int(cls.day) <= 31 and 1 <= int(cls.month) and cls.year < 9999:
             return True
@@ -26,7 +26,7 @@ class Date():
             return False
 
     @classmethod
-    def from_string(cls, date : str) -> 'Date':
+    def from_string(cls, date: str) -> 'Date':
         cls.split_date(date)
         finish_date = cls(cls.day, cls.month, cls.year)
         return finish_date
@@ -37,31 +37,4 @@ print(date)
 print(Date.is_date_valid('10-12-2077'))
 print(Date.is_date_valid('40-12-202020'))
 
-
-'''
-Задача 3. Дата
-Что нужно сделать
-Реализуйте класс Date, который должен:
-
-проверять числа даты на корректность;
-конвертировать строку даты в объект класса Date, состоящий из соответствующих числовых значений дня, месяца и года.
-Оба метода должны получать на вход строку вида ‘dd-mm-yyyy’.
-
-При тестировании программы объект класса Date должен инициализироваться исключительно через метод конвертации,
-например:
-
-date = Date.from_string('10-12-2077')
-Неверный вариант: date = Date(10, 12, 2077)
-
-Пример основного кода:
-
-date = Date.from_string('10-12-2077')
-print(date)
-print(Date.is_date_valid('10-12-2077'))
-print(Date.is_date_valid('40-12-2077'))
-
-Результат:
-День: 10    Месяц: 12    Год: 2077
-True
-False
-'''
+# зачтено
