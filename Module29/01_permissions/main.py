1afdfe3171291bc6719e5dd2d9949e50bcc866dd
Игорь Е с работы <toermakov@mail.ru>
2023-05-23 16:36:59 +0300

    task01_inProcess - проработать права пользователя

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,66 @@
 # TODO здесь писать код
+from typing import Callable
+from functools import wraps
+def check_permission(username : str) -> Callable:
+    """
+        Декоратор проверки прав пользователя.
+        Возвращает ошибку или право доступа к функции
+        """
+    def check_permission_down(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Callable:
+            try:
+                if username == 'admin':
+                    return func(*args, **kwargs)
+                else:
+                    raise PermissionError
+            except PermissionError:
+                print('PermissionError: У Вас недостаточно прав. Обратитесь к администратору')
+        return wrapper
+    return check_permission_down
+
+# user_permissions = ['admin']
+# user_permissions_u = ['user']
+
+@check_permission('admin')
+# @check_permission('user_1')
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission('user_1')
+# @check_permission('admin')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
+
+
+'''
+Задача 1. Права доступа
+Что нужно сделать
+Перед вами стоит задача создать и поддерживать специализированный форум. Вы только приступили и сейчас 
+работаете над действиями, которые могут совершать посетители форума. 
+Для разных пользователей прописаны разные возможности.
+
+Напишите декоратор check_permission, который проверяет, есть ли у пользователя доступ к вызываемой функции, 
+и если нет, то выдаёт исключение PermissionError.
+
+Пример кода:
+user_permissions = ['admin']
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+delete_site()
+add_comment()
+Результат:
+Удаляем сайт
+PermissionError: у пользователя недостаточно прав, чтобы выполнить функцию add_comment
+'''
