aebc3cbb0be028500ec140a01448ddeed2685c2e
Игорь Е с работы <toermakov@mail.ru>
2023-05-25 12:00:44 +0300

    task02_comlete

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,28 +1,37 @@
 # TODO здесь писать код
-
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 app = dict()
 
-def callback():
+def callback(route : str) -> Callable:
     """
     Это функция, которая вызывается при срабатывании определённого события: переходе на страницу,
     получении сообщения или окончании обработки процессором
     """
-    pass
+    def decorator_callback(func: Callable) -> Callable:
+        app[route] = func
+
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator_callback
 
 @callback('//')
 def example():
     print('Пример функции, которая возвращает ответ сервера')
     return 'OK'
 
-#
 route = app.get('//')
 if route:
     response = route()
     print('Ответ:', response)
 else:
     print('Такого пути нет')
+
 
 
 '''
