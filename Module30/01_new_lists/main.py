2519fc879d1de387b3b72c7e1d0f727ae5b46f0f
Nikita Fomin <fominquant@yandex.ru>
2023-05-26 17:42:11 +0300

    Module 30 homework passed

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,6 +1,7 @@
 # TODO здесь писать код
-from typing import List
 from functools import reduce
+from typing import List
+
 
 def my_multiply(a: int, b: int) -> int:
     result = a * b
@@ -11,26 +12,13 @@ def my_multiply(a: int, b: int) -> int:
 floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
 names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
 numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
-#_________________________________________________
+# _________________________________________________
 
-my_floats : List[float] = list(map(lambda x : round(x ** 3, 3), floats))
-my_names : List[str]    = list(filter(lambda letter: len(letter) >=5, names))
+my_floats: List[float] = list(map(lambda x: round(x ** 3, 3), floats))
+my_names: List[str] = list(filter(lambda letter: len(letter) >= 5, names))
 
 print('Задание 1:', my_floats)
 print('Задание 2:', my_names)
 print('Задание 3:', reduce(my_multiply, numbers))
 
-'''
-Задача 1. Новые списки
-Что нужно сделать
-Даны три списка: 
-
-floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
-names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
-numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
-Напишите код, который создаёт три новых списка. Вот их содержимое:
-
-Каждое число из списка floats возводится в третью степень и округляется до трёх знаков после запятой.
-Из списка names берутся только имена минимум из пяти букв.
-Из списка numbers берётся произведение всех чисел.
-'''
+# зачтено
--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,29 +1,12 @@
 # TODO здесь писать код
 from collections import Counter
 
-def can_be_poly(letters : str) -> bool:
+
+def can_be_poly(letters: str) -> bool:
     return sum(x % 2 for x in Counter(letters).values()) <= 1
 
+
 print(can_be_poly('abcba'))
 print(can_be_poly('abbbc'))
 
-
-'''
-Задача 3. Палиндром: возвращение
-Что нужно сделать
-Есть множество встроенных и внешних библиотек для работы с данными в Python. С некоторыми из них вы уже работали.
- Например, с модулем collections, когда использовали специальный класс OrderedDict, 
- с помощью которого делали упорядоченный словарь. В этой библиотеке есть и другие возможности, но их немного. 
-Официальная документация: collections — Container datatypes.
-
-Используя модуль collections, реализуйте функцию can_be_poly, которая принимает на вход строку и проверяет,
- можно ли получить из неё палиндром. 
-
-Пример кода:
-print(can_be_poly('abcba'))
-print(can_be_poly('abbbc'))
-
-Результат:
-True
-False
-'''
+# зачтено
