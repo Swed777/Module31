61fe8a76095b092d7f68899165fe1e8940bb9db1
Игорь <toermakov@mail.ru>
2023-05-27 01:35:44 +0300

    task02_complete

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -4,7 +4,6 @@ from typing import List
 
 number_str : str = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
 
-
 priv_number : List[str]= re.findall(r'\b[АВЕКМНОРСТУХ]\d{3}\w{2}\d{3}', number_str)
 taxi_number : List[str] = re.findall(r'\b[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}', number_str)
 
