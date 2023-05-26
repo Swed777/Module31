1e7cf942daebdb44dd9ca06999c75e69101b0c96
Игорь Е с работы <toermakov@mail.ru>
2023-05-26 15:46:03 +0300

    task01_complete

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,5 +1,12 @@
 # TODO здесь писать код
 from typing import List
+from functools import reduce
+
+def my_multiply(a: int, b: int) -> int:
+    result = a * b
+    # print(f"{a} * {b} = {result}")        # Оставил на память - ЕИ
+    return result
+
 
 floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
 names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
@@ -7,12 +14,11 @@ numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
 #_________________________________________________
 
 my_floats : List[float] = list(map(lambda x : round(x ** 3, 3), floats))
-my_names : List[str] = list(filter(lambda letter: len(letter) >=5, names))
-
-
+my_names : List[str]    = list(filter(lambda letter: len(letter) >=5, names))
 
 print('Задание 1:', my_floats)
 print('Задание 2:', my_names)
+print('Задание 3:', reduce(my_multiply, numbers))
 
 '''
 Задача 1. Новые списки
