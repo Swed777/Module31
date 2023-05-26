895116b0355a1731876b90828847ef99b0d9d4bf
Игорь Е с работы <toermakov@mail.ru>
2023-05-26 16:37:50 +0300

    task03_complete

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,12 +1,12 @@
 # TODO здесь писать код
+from collections import Counter
 
 def can_be_poly(letters : str) -> bool:
-    pass
-
-
+    return sum(x % 2 for x in Counter(letters).values()) <= 1
 
 print(can_be_poly('abcba'))
 print(can_be_poly('abbbc'))
+
 
 '''
 Задача 3. Палиндром: возвращение
