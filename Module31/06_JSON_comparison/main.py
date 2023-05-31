14616403d3eabeb11efa27d2e14c3007a10a82ca
Игорь <toermakov@mail.ru>
2023-06-01 01:04:12 +0300

    task06_inProcess

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -1,22 +1,18 @@
 # TODO здесь писать код
 from typing import List, Dict
 import json
-diff_list : List[str] = ['services', 'staff', 'datetime']   # Список параметров для отслеживания
+result = dict()
+diff_list: List[str] = ['services', 'staff', 'datetime']  # Список параметров для отслеживания
 def find_param(file : Dict, diff_list : List):
-    # for param in diff_list:
     for i_key, value in file.items():
         for param in diff_list:
             if param == i_key:
-                print('Словарь из дифлиста', i_key, value)
-            else:
-                if not isinstance(value, dict):
-                    pass
-                else:
-                    print('конечный елсе', i_key, value)
-                    find_param(value, diff_list)
+                result[i_key] = value
+                diff_list.remove(param)
+            elif isinstance(value, dict):
+                find_param(value, diff_list)
+    return result
 
-
-result = dict()                                               # вывод в консоль - Словарь {параметр: новое_значение, ….}
 
 with open('json_old.json', 'r') as old_file:
     o_file : Dict = json.load(old_file)
@@ -24,8 +20,15 @@ with open('json_old.json', 'r') as old_file:
 with open('json_new.json', 'r') as new_file:
     n_file = json.load(new_file)
     # print(n_file)
-print('Файлы идентичны' if o_file == n_file else  'Файлы разные, ищем разницу в значениях:')
+print('Файлы идентичны' if o_file == n_file else 'Файлы разные, ищем разницу в значениях:')
 print('*' * 43, end='\n')
+
+print(find_param(o_file, diff_list))
+
+
+# with open('result.json', 'w') as result_file:
+#     json.dump(o_file, result_file, indent=4)
+
 
 
 # for key, o_items in o_file.items():
@@ -37,14 +40,7 @@ print('*' * 43, end='\n')
 #             # print('Значение в новом файле',  n_items)
 #             pass
 
-find_param(o_file, diff_list)
 
-
-
-
-
-# with open('result.json', 'w') as result_file:
-#     json.dump(o_file, result_file, indent=4)
 
 '''
 Задача 6. Поиск разницы между двумя JSON-файлами (пример из реального тестового задания на должность Python-разработчика уровня Junior)
