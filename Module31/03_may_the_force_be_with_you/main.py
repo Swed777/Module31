13663281e4765ff53555a95385b52d411bbf9e99
Игорь Е с работы <toermakov@mail.ru>
2023-06-02 13:19:52 +0300

    task03_inStart

--- Module31/03_may_the_force_be_with_you/main.py
+++ Module31/03_may_the_force_be_with_you/main.py
@@ -1,14 +1,32 @@
 # TODO здесь писать код
 import requests
+import json
 import re
 
+'Тестирую'
 
+# war_api = requests.get('http://swapi.dev/api/planets/1/')
+# param = {'ship_name':'Millennium Falcon'}
+# war_api = requests.get('https://swapi.dev/api/people/')
 
+# war_api = requests.get("https://api.thedogapi.com/v1/breeds/1")
+# print(war_api.text)
+# print(war_api.content)
+# # print(war_api.headers)
+# war_api.headers.get('Content-Type')
+# print(war_api.json())
+# print(war_api.json())
+# for k,v in war_api.items():
+#     print('-->', v, end='\n')
 
+api_key = "IxvocnRc69rLkVZUOrPg47UzMlTjghXdvSoHqzzi"
+query_params = {"api_key": api_key, "earth_date": "2020-07-01"}
+# nasa_api = requests.get('https://api.nasa.gov/')
+nasa_foto = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos", params=query_params)
+print(nasa_foto.json())
+foto = nasa_foto.json()['photos'][5]['img_src']
+print(foto)
 
-
-
-pass
 '''
 Задача 3. May the force be with you
 Что нужно сделать
